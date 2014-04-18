92fec03c3b10c807b2ac771f17838fc3c5a2ce19
Brett Lischalk <brett@brettlischalk.com>
2014-04-18 17:16:20 -0500

    Adding midje.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.3.1"]]
+  :profiles {:dev {:plugins [[lein-midje "3.0.0"]]
+                   :dependencies [[midje "1.6.3"]]}})
--- test/git_log_parser/core_test.clj
+++ test/git_log_parser/core_test.clj
@@ -1,7 +1,8 @@
 (ns git-log-parser.core-test
   (:require [clojure.test :refer :all]
-            [git-log-parser.core :refer :all]))
+            [git-log-parser.core :refer :all]
+            [midje.sweet :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Passing test"
+    (is (= 1 1))))
