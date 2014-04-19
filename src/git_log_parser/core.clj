8ded746e3a6c13d7c5c3091e9cc39d8512444b52
Brett Lischalk <brett@brettlischalk.com>
2014-04-19 14:49:44 -0500

    Attempting to use instaexample instaparse.

--- src/git_log_parser/core.clj
+++ src/git_log_parser/core.clj
@@ -1,7 +1,27 @@
 (ns git-log-parser.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta])
   (:gen-class :main true))
 
+(def commit
+  (insta/parser "<COMMITS> = <NEWLINE*> COMMIT+
+                              SHA = #'[a-f0-9]+'
+                              COMMIT = (<'commit '> SHA <NEWLINE>
+                                       AUTHOR <NEWLINE>
+                                       DATE <NEWLINE>
+                                       <NEWLINE>
+                                       BODY)
+                              BODY = SPACE+ STORY <COLON?> SPACE BODY-LINE+
+                              STORY = <'PD-'> #'\\d+'
+                              BODY-LINE = <SPACE*> (WORD (SPACE+ WORD)*)? <NEWLINE>
+                              <AUTHORNAME> = #'[a-zA-Z\\- \\.]+'
+                              AUTHOR = <'Author:' SPACE+> WORD (SPACE WORD)*
+                              <WORD> = #'\\S+' | HAPPINESS
+                              HAPPINESS = <'h:'> #'[0-5]'
+                              <SPACE> = <' '>
+                              <COLON> = ':'
+                              DATE = <'Date:' SPACE+> WORD (SPACE WORD)*
+                              NEWLINE = '\n'"))
 
 (defn -main [& args]
   (with-open  [input (io/reader *in*)
@@ -9,5 +29,4 @@
     (do
       (.write
        output
-       (apply str (line-seq input))))))
-
+       (apply str (commit (apply str (line-seq input))))))))
