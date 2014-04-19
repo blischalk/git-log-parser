ff9200914bd9c3d30759fc0062fcc0433f8369ac
Brett Lischalk <brett@brettlischalk.com>
2014-04-18 20:03:21 -0500

    Read from input stream and print ln.

--- src/git_log_parser/core.clj
+++ src/git_log_parser/core.clj
@@ -2,6 +2,11 @@
   (:require [clojure.java.io :as io])
   (:gen-class :main true))
 
-(defn -main
-  [& args]
-  (println args))
+
+(defn -main [& args]
+  (with-open  [stream
+               (io/reader (. System in))]
+    (do
+      (println
+       (apply str (line-seq stream))))))
+
